29a3b9afc54bc8261809ba1d17e0cdab58b18607
neckaros <neckaros@gmail.com>
2024-04-04 23:46:52 +0200

    add rusqlite for resolution and audio

--- src/rusqlite.rs
+++ src/rusqlite.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 
 use rusqlite::{types::{FromSql, FromSqlError, FromSqlResult, ToSqlOutput, ValueRef}, ToSql};
 
-use crate::{PluginType, CredentialType};
+use crate::{CredentialType, PluginType, RsAudio, RsResolution};
 
 impl FromSql for PluginType {
     fn column_result(value: ValueRef) -> FromSqlResult<Self> {
@@ -33,6 +33,43 @@ impl FromSql for CredentialType {
 }
 
 impl ToSql for CredentialType {
+    fn to_sql(&self) -> rusqlite::Result<rusqlite::types::ToSqlOutput<'_>> {
+        let l = &self.clone();
+        let r = l.to_string();
+        Ok(ToSqlOutput::from(r))
+    }
+}
+
+
+
+
+impl FromSql for RsResolution {
+    fn column_result(value: ValueRef) -> FromSqlResult<Self> {
+        String::column_result(value).and_then(|as_string| {
+            let r = RsResolution::from_str(&as_string).map_err(|_| FromSqlError::InvalidType);
+            r
+        })
+    }
+}
+
+impl ToSql for RsResolution {
+    fn to_sql(&self) -> rusqlite::Result<rusqlite::types::ToSqlOutput<'_>> {
+        let l = &self.clone();
+        let r = l.to_string();
+        Ok(ToSqlOutput::from(r))
+    }
+}
+
+impl FromSql for RsAudio {
+    fn column_result(value: ValueRef) -> FromSqlResult<Self> {
+        String::column_result(value).and_then(|as_string| {
+            let r = RsAudio::from_str(&as_string).map_err(|_| FromSqlError::InvalidType);
+            r
+        })
+    }
+}
+
+impl ToSql for RsAudio {
     fn to_sql(&self) -> rusqlite::Result<rusqlite::types::ToSqlOutput<'_>> {
         let l = &self.clone();
         let r = l.to_string();

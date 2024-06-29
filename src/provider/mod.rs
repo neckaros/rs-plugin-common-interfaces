ac3a9112bbdebf8b4302860b4f50bf81472b03f5
neckaros <neckaros@gmail.com>
2024-06-29 16:56:41 +0200

    provder and remove range

--- src/provider/mod.rs
+++ src/provider/mod.rs
@@ -3,17 +3,15 @@
 // remove(path: RsProviderPath) -> bool;
 // infos(path: RsProviderPath) -> MediaForUpdate;
 // get_file(path: RsProviderPath) -> RsRequest;
-// get_file_write_stream(path: RsProviderPath) -> RsRequest;
+// add_file(path: RsProviderPath) -> RsRequest;
 
 use serde::{Deserialize, Serialize};
 
-use crate::RsRangeDefinition;
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 #[serde(rename_all = "camelCase")] 
 pub struct RsProviderPath {
     pub root: String,
     pub source: String,
-    pub range: Option<RsRangeDefinition>
 }
 

a4f6af4f7d3109fbf95d0951bba87af660dfbc75
neckaros <arnaud.jezequel@gmail.com>
2025-02-12 15:47:11 +0100

    extend id

--- src/domain/rs_ids.rs
+++ src/domain/rs_ids.rs
@@ -1,6 +1,26 @@
 use serde::{Deserialize, Serialize};
 
 
+#[derive(Debug, Serialize, strum_macros::AsRefStr)]
+pub enum RsIdsError {
+    InvalidId(),
+	NotAMediaId(String),
+    NoMediaIdRequired(Box<RsIds>)
+
+}
+
+// region:    --- Error Boilerplate
+
+impl core::fmt::Display for RsIdsError {
+	fn fmt(
+		&self,
+		fmt: &mut core::fmt::Formatter,
+	) -> core::result::Result<(), core::fmt::Error> {
+		write!(fmt, "{self:?}")
+	}
+}
+
+impl std::error::Error for RsIdsError {}
 
 #[derive(Debug, Clone, Serialize, Deserialize, Ord, PartialOrd, Eq, PartialEq, Default)]
 #[serde(rename_all = "camelCase")]
@@ -21,6 +41,131 @@ pub struct RsIds {
     pub tvrage: Option<u64>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub other_ids: Option<Vec<String>>,
+}
+
+
+
+impl RsIds {
+    pub fn try_add(&mut self, value: String) -> Result<(), RsIdsError> {
+        if !Self::is_id(&value) {
+            return Err(RsIdsError::NotAMediaId(value))
+        }
+        let elements = value.split(":").collect::<Vec<_>>();
+        let source = elements.first().ok_or(RsIdsError::InvalidId())?;
+        let id = elements.get(2).ok_or(RsIdsError::InvalidId())?;
+
+        if *source == "redseat" {
+            self.redseat = Some(id.to_string());
+            Ok(())
+        } else if *source == "imdb" {
+            self.imdb = Some(id.to_string());
+            Ok(())
+        } else if *source == "trakt" {
+            let id: u64 = id.parse().map_err(|_| RsIdsError::NotAMediaId(value))?;
+            self.trakt = Some(id);
+            Ok(())
+        } else if *source == "tmdb" {
+            let id: u64 = id.parse().map_err(|_| RsIdsError::NotAMediaId(value))?;
+            self.tmdb = Some(id);
+            Ok(())
+        } else if *source == "tvdb" {
+            let id: u64 = id.parse().map_err(|_| RsIdsError::NotAMediaId(value))?;
+            self.tvdb = Some(id);
+            Ok(())
+        } else if *source == "tvrage" {
+            let id: u64 = id.parse().map_err(|_| RsIdsError::NotAMediaId(value))?;
+            self.tvrage = Some(id);
+            Ok(())
+        } else{
+            Err(RsIdsError::NotAMediaId(value))
+        }  
+    }
+
+    pub fn into_best(self) -> Option<String> {
+        self.as_redseat().or(self.into_best_external())
+    }
+
+    pub fn into_best_external(self) -> Option<String> {
+        self.as_trakt().or(self.as_imdb()).or(self.as_tmdb()).or(self.as_tvdb())
+    }
+    pub fn as_best_external(&self) -> Option<String> {
+        self.as_trakt().or(self.as_imdb()).or(self.as_tmdb()).or(self.as_tvdb())
+    }
+
+
+    pub fn into_best_external_or_local(self) -> Option<String> {
+        self.as_best_external().or(self.as_redseat())
+    }
+
+
+    pub fn from_imdb(imdb: String) -> Self {
+        Self {
+            imdb: Some(imdb),
+            ..Default::default()
+        }
+    }
+    pub fn as_imdb(&self) -> Option<String> {
+        self.imdb.as_ref().map(|i| format!("imdb:{}", i))
+    }
+    
+    pub fn from_trakt(trakt: u64) -> Self {
+        Self {
+            trakt: Some(trakt),
+            ..Default::default()
+        }
+    }
+    pub fn as_trakt(&self) -> Option<String> {
+        self.trakt.map(|i| format!("trakt:{}", i))
+    }
+
+    pub fn from_tvdb(tvdb: u64) -> Self {
+        Self {
+            tvdb: Some(tvdb),
+            ..Default::default()
+        }
+    }
+    pub fn as_tvdb(&self) -> Option<String> {
+        self.tvdb.map(|i| format!("tvdb:{}", i))
+    }
+
+    pub fn from_tmdb(tmdb: u64) -> Self {
+        Self {
+            tmdb: Some(tmdb),
+            ..Default::default()
+        }
+    }
+    pub fn as_tmdb(&self) -> Option<String> {
+        self.tmdb.map(|i| format!("tmdb:{}", i))
+    }
+
+    pub fn from_redseat(redseat: String) -> Self {
+        Self {
+            redseat: Some(redseat),
+            ..Default::default()
+        }
+    }
+    pub fn as_redseat(&self) -> Option<String> {
+        self.redseat.as_ref().map(|i| format!("redseat:{}", i))
+    }
+
+    pub fn as_id(&self) -> Result<String, RsIdsError> {
+        if let Some(imdb) = &self.imdb {
+            Ok(format!("imdb:{}", imdb))
+        } else if let Some(trakt) = &self.trakt {
+            Ok(format!("trakt:{}", trakt))
+        } else if let Some(tmdb) = &self.tmdb {
+            Ok(format!("tmdb:{}", tmdb))
+        } else if let Some(tvdb) = &self.tvdb {
+            Ok(format!("tmdb:{}", tvdb))
+        } else {
+            Err(RsIdsError::NoMediaIdRequired(Box::new(self.clone())))
+        }
+    }   
+
+    /// check if the provided id need parsing like "trakt:xxxxx" and is not directly the local id from this server
+    pub fn is_id(id: &str) -> bool {
+        id.contains(":") && id.split(":").count() == 2
+    }
 }
 
 

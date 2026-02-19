dc8c237441b75343dac715dd83e039078546352b
Arnaud JEZEQUEL <arnaud.jezequel@gmail.com>
2026-02-19 14:22:20 +0100

    tag for update

--- src/domain/tag.rs
+++ src/domain/tag.rs
@@ -21,6 +21,28 @@ pub struct Tag {
     pub path: String,
 }
 
+
+
+#[derive(Debug, Serialize, Deserialize, Clone, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct TagForUpdate {
+	pub name: Option<String>,
+    pub parent: Option<String>,
+    #[serde(rename = "type")]
+    pub kind: Option<String>,
+    
+    pub alt: Option<Vec<String>>,
+    pub add_alts: Option<Vec<String>>,
+    pub remove_alts: Option<Vec<String>>,
+
+    pub thumb: Option<String>,
+    pub params: Option<Value>,
+    pub generated: Option<bool>,
+    
+    pub migrate_to: Option<String>,
+}
+
+
 impl Tag {
     pub fn full_path(&self) -> String {
         format!("{}{}", self.path, self.name)

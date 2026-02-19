3f0097ac320bdf43f08048ea081f34cbe8c548f0
Arnaud JEZEQUEL <arnaud.jezequel@gmail.com>
2026-02-19 12:09:51 +0100

    book update

--- src/domain/book.rs
+++ src/domain/book.rs
@@ -66,6 +66,37 @@ impl From<Book> for RsIds {
     }
 }
 
+#[derive(Debug, Serialize, Deserialize, Clone, Default, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct BookForUpdate {
+    pub name: Option<String>,
+    #[serde(rename = "type")]
+    pub kind: Option<String>,
+    pub serie_ref: Option<String>,
+    pub volume: Option<f64>,
+    pub chapter: Option<f64>,
+    pub year: Option<u16>,
+    pub airdate: Option<i64>,
+    pub overview: Option<String>,
+    pub pages: Option<u32>,
+    pub params: Option<Value>,
+    pub lang: Option<String>,
+    pub original: Option<String>,
+    pub isbn13: Option<String>,
+    pub openlibrary_edition_id: Option<String>,
+    pub openlibrary_work_id: Option<String>,
+    pub google_books_volume_id: Option<String>,
+    pub asin: Option<String>,
+    pub otherids: Option<OtherIds>,
+}
+
+impl BookForUpdate {
+    pub fn has_update(&self) -> bool {
+        self != &BookForUpdate::default()
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::Book;
